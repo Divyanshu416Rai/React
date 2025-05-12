d828e3e8478c6c3924a4c3eb720a3763eb609da1
Divyanshu416Rai <divyanshukumarrai832@gmail.com>
2025-05-12 15:58:56 +0530

    first commit

---
+++ React.js
@@ -0,0 +1,6 @@
+//REACT :React JS is an open-source JavaScript library used for building user interfaces (UIs), especially for single-page applications (SPAs) where performance and dynamic updates are important.
+
+//Framework : A framework is a pre-defined structure or platform that provides a foundation for developing software applications. It gives you a set of tools, rules, and guidelines so you can build and organize your project more easily
+
+//Virtual Dom :The Virtual DOM is a lightweight, in-memory copy of the real DOM (Document Object Model). React uses it to track changes and update the UI efficiently.
+
